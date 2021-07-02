3c0a2b3d65f7c0b6f335cd0b023b80d742ef0516
nsiegner <nick.siegner@gmail.com>
2021-07-02 16:16:58 +0200

    re aranging the idea for 2048

--- 2048/Game.py
+++ 2048/Game.py
@@ -28,70 +28,7 @@ class Game(object):
         else:
             add_tile_4()
 
-    def swipe_left():
 
-        new_grid = [[0]*4 for i in range(4)]
-
-
-        for i in range(4):
-            pos = 0
-            for j in range(4):
-                if grid [i][j] != 0:
-                    new_grid[i][pos] = self.grid[i][j]
-                    pos += 1
-
-        self.grid = new_grid
-
-    def merge(self):
-
-
-        for i in range(4):
-            for j in range(3):
-
-
-                if(self.grid[i][j] == self.grid[i][j + 1] and self.grid[i][j] != 0):
-
-                    self.grid[i][j] = self.grid[i][j] * 2
-                    self.grid[i][j + 1] = 0
-
-
-    def invert():
-        new_grid = []
-        for i in range(4):
-            new_grid.append([])
-            for j in range(4):
-            new_grid[i].append(self.grid[i][3 - j])
-
-        self.grid = new_grid
-
-    def transpose():
-        new_grid = []
-        for i in range(4):
-            new_grid.append([])
-            for j in range(4):
-                new_grid[i].append(self.grid[i][j])
-
-        self.grid = new_grid
-
-    def move_left():
-        self.swipe_left()
-        self.merge()
-        self.swipe_left()
-
-    def move_right():
-        self.invert()
-        self.move_left()
-        self.invert()
-
-    def move_up():
-        self.transpose()
-        self.move_left()
-        self.transpose()
-
-    def move_down():
-        self.transpose()
-        self.move_right()
-        self.transpose()
 
     def get_current_state():
         for i in range(4):
