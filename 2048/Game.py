6aac19d61dffd4e77d448346e4895361a606eeab
nsiegner <nick.siegner@gmail.com>
2021-07-02 23:46:18 +0200

    write the first move method for the game class

--- 2048/Game.py
+++ 2048/Game.py
@@ -1,14 +1,17 @@
 import tkinter as tk
-import View as view
-import Random as rnd
+import random as rnd
 class Game(object):
 
 
     def __init__(self):
         self.grid = [[None]*4 for i in range(4)]
-        add_tile_2()
-        add_tile_2()
-        view.draw()
+        self.add_tile_2()
+        self.add_tile_2()
+        
+
+
+    def set_grid(self, grid):
+        self.grid = grid
 
 
 
@@ -28,7 +31,31 @@ class Game(object):
         else:
             add_tile_4()
 
+    def push_up():
+        for i in range(1, 4):
+            for j in range(4):
+                if self.grid[i][j] != 0:
+                    if self.grid[0][j] == 0:
+                        self.grid[0][j] = self.grid[i][j]
+                        self.grid[i][j] = 0
+                    elif self.grid[1][j] == 0:
+                        self.grid[1][j] = self.grid[i][j]
+                        self.grid[i][j] = 0
+                    elif self.grid[2][j] == 0:
+                        self.grid[2][j] = self.grid[i][j]
+                        self.grid[i][j] = 0
 
+    def merge():
+            for i in range(3):
+                for j in range(4):
+                    if self.grid[i + 1][j] == self.grid[i][j]:
+                        self.grid[i][j] *= 2
+                        self.grid[i + 1][j] = 0
+
+    def move_up():
+        self.push_up()
+        self.merge()
+        self.push_up()
 
     def get_current_state():
         for i in range(4):
@@ -36,13 +63,13 @@ class Game(object):
                 if grid[i][j] == 2048:
                     return "Won"
 
-                elif self.grid[i][j] = 0:
+                elif self.grid[i][j] == 0:
                     return "Not done jet"
 
         for i in range(3):
             for j in range(3):
                 if grid[i][j] == grid[i + 1][j] or grid[i][j] == grid[i][j + 1]:
-                    retrun "Not done jet"
+                    return "Not done jet"
 
         for i in range(3):
             if grid[i][3] == grid[i + 1][3]:
