b4f99aec30bc85c12b4e20de3e7a88ff96646309
nsiegner <nick.siegner@gmail.com>
2021-07-02 23:46:43 +0200

    begin writing the test for the move_up method of the game class

--- 2048/Test_game.py
+++ 2048/Test_game.py
@@ -1,12 +1,17 @@
 import unittest
-import Game as game
+from Game import *
 class test_game(unittest.TestCase):
-    def setUp():
-        game = Game()
-        game.show_grid()
+    def setUp(self):
+        self.game = Game()
 
-    def test_move_up():
-        pass
+
+    def test_move_up(self):
+        example1 = [[2, 0, 0, 2], [2, 2, 2, 0], [2, 0, 0, 2], [2, 2, 4, 4]]
+        soultion1 = [[4, 4, 2, 4], [4, 0, 4, 4], [0, 0, 0, 0], [0, 0, 0, 0]]
+        self.game.set_grid(example1)
+        self.game.move_up()
+        self.assertEqual(soultion1, game.grid)
 
 if __name__ == "__main__":
-    test_game.unittest()
+    test_game.setUp()
+    test_game.test_move_up()
