2d8fd5404f5c4c3b6312c2077b6631045828b4ad
nsiegner <nick.siegner@gmail.com>
2021-07-03 18:12:47 +0200

    writing tests for the move methods

--- 2048/Test_game.py
+++ 2048/Test_game.py
@@ -6,12 +6,105 @@ class test_game(unittest.TestCase):
 
 
     def test_move_up(self):
-        example1 = [[2, 0, 0, 2], [2, 2, 2, 0], [2, 0, 0, 2], [2, 2, 4, 4]]
-        soultion1 = [[4, 4, 2, 4], [4, 0, 4, 4], [0, 0, 0, 0], [0, 0, 0, 0]]
+        example1 = [[2, 0, 0, 2],
+                    [2, 2, 2, 0],
+                    [2, 0, 0, 2],
+                    [2, 2, 4, 4]]
+
+        soultion1 = [[4, 4, 2, 4],
+                     [4, 0, 4, 4],
+                     [0, 0, 0, 0],
+                     [0, 0, 0, 0]]
+
         self.game.set_grid(example1)
         self.game.move_up()
-        self.assertEqual(soultion1, game.grid)
+        self.assertEqual(soultion1, self.game.get_grid())
+
+    def test_move_down(self):
+        example1 = [[2, 2, 4, 4],
+                    [2, 0, 0, 2],
+                    [2, 2, 2, 0],
+                    [2, 0, 0, 2]]
+
+        soultion1 = [[0, 0, 0, 0],
+                     [0, 0, 0, 0],
+                     [4, 0, 4, 4],
+                     [4, 4, 2, 4]]
+
+        self.game.set_grid(example1)
+        self.game.move_down()
+        self.assertEqual(soultion1, self.game.get_grid())
+
+    def test_move_left(self):
+        example1 = [[2, 2, 2, 2],
+                    [2, 0, 2, 0],
+                    [4, 0, 2, 0],
+                    [4, 2, 0, 2]]
+
+        soultion1 = [[4, 4, 0, 0],
+                     [4, 0, 0, 0],
+                     [4, 2, 0, 0],
+                     [4, 4, 0, 0]]
+        self.game.set_grid(example1)
+        self.game.move_left()
+        self.assertEqual(soultion1, self.game.get_grid())
+
+    def test_move_right(self):
+        example1 = [[2, 2, 2, 2],
+                    [2, 0, 2, 0],
+                    [4, 0, 2, 0],
+                    [4, 2, 0, 2]]
+
+        soultion1 = [[0, 0, 4, 4],
+                     [0, 0, 0, 4],
+                     [0, 0, 4, 2],
+                     [0, 0, 4, 4]]
+
+        self.game.set_grid(example1)
+        self.game.move_right()
+        self.assertEqual(soultion1, self.game.get_grid())
+
+    def test_invert(self):
+        example1 = [[2, 0, 2, 4],
+                    [2, 0, 0, 0],
+                    [8, 4, 0, 8],
+                    [8, 4, 0, 2]]
+
+        soultion1 = [[8, 4, 0, 2],
+                     [8, 4, 0, 8],
+                     [2, 0, 0, 0],
+                     [2, 0, 2, 4]]
+        self.game.set_grid(example1)
+        self.game.invert()
+        self.assertEqual(soultion1, self.game.get_grid())
+
+    def test_rotate_right(self):
+        example1 = [[2, 0, 2, 4],
+                    [2, 0, 0, 0],
+                    [8, 4, 0, 8],
+                    [8, 4, 0, 2]]
+
+        soultion1 = [[8, 8, 2, 2],
+                     [4, 4, 0, 0],
+                     [0, 0, 0, 2],
+                     [2, 8, 0, 4]]
+        self.game.set_grid(example1)
+        self.game.rotate_right()
+        self.assertEqual(soultion1, self.game.get_grid())
+
+    def test_rotate_left(self):
+        example1 = [[2, 0, 2, 4],
+                    [2, 0, 0, 0],
+                    [8, 4, 0, 8],
+                    [8, 4, 0, 2]]
+
+        soultion1 = [[4, 0, 8, 2],
+                     [2, 0, 0, 0],
+                     [0, 0, 4, 4],
+                     [2, 2, 8, 8]]
+        self.game.set_grid(example1)
+        self.game.rotate_left()
+        self.assertEqual(soultion1, self.game.get_grid())
 
 if __name__ == "__main__":
-    test_game.setUp()
-    test_game.test_move_up()
+    unittest.main()
