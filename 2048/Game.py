803693855b506b2f6f94ad6f272afc9446480917
nsiegner <nick.siegner@gmail.com>
2021-07-04 01:19:33 +0200

    implementing the rest of the game logic and visualisation on the consol

--- 2048/Game.py
+++ 2048/Game.py
@@ -5,39 +5,39 @@ class Game(object):
 
 
     def __init__(self):
-        self.grid = [[None]*4 for i in range(4)]
+        self.grid = [[0]*4 for i in range(4)]
         self.add_tile_2()
         self.add_tile_2()
 
-        '''if input("do you want to play?").lower() == "yes":
-            self.main_loop()
-        else:
-            pass
+        self.main_loop()
 
     def main_loop(self):
-        grid_before = self.grid
         self.show_grid()
-        x = input("Which direction you want to go?")
-        if x.lower() == "w":
+        new_grid = self.grid
+
+        x = input("which direction you want to go in:").lower()
+        if x == "w":
             self.move_up()
-        elif x.lower() == "s":
+        elif x == "s":
             self.move_down()
-        elif x.lower() == "a":
+        elif x == "a":
             self.move_left()
-        elif x.lower() == "d":
+        elif x == "d":
             self.move_right()
         else:
             pass
-        if grid_before != self.grid:
-             if self.is_full != True:
-                 self.add_tile()
+
+        if self.is_full() != True and new_grid != self.grid:
+            self.add_tile()
 
         if self.get_current_state() == "Not done jet":
             self.main_loop()
         elif self.get_current_state() == "Won":
-            pass
+            print("You have won")
         elif self.get_current_state() == "Game Over":
-            pass'''
+            print("Game Over")
+
+
 
     def set_grid(self, grid):
         self.grid = grid
@@ -46,16 +46,16 @@ class Game(object):
         return self.grid
 
     def add_tile(self):
-        for i in range(10):
-            if i == 0 or i == 1:
-                self.add_tile_4()
-            else:
-                self.add_tile_2()
+        i = rnd.randint(0, 10)
+        if i == 0 or i == 1:
+            self.add_tile_4()
+        else:
+            self.add_tile_2()
 
     def add_tile_2(self):
         x = rnd.randint(0, 3)
         y = rnd.randint(0, 3)
-        if self.grid[x][y] == None:
+        if self.grid[x][y] == 0:
             self.grid[x][y] = 2
         else:
             self.add_tile_2()
@@ -63,7 +63,7 @@ class Game(object):
     def add_tile_4(self):
         x = rnd.randint(0, 3)
         y = rnd.randint(0, 3)
-        if self.grid[x][y] == None:
+        if self.grid[x][y] == 0:
             self.grid[x][y] = 4
         else:
             self.add_tile_4()
@@ -85,8 +85,8 @@ class Game(object):
     def merge(self):
             for i in range(3):
                 for j in range(4):
-                    if self.grid[i + 1][j] == self.grid[i][j]:
-                        self.grid[i][j] *= 2
+                    if self.grid[i + 1][j] == self.grid[i][j] and self.grid[i][j] != None:
+                        self.grid[i][j] = self.grid[i][j]*2
                         self.grid[i + 1][j] = 0
 
     def invert(self):
@@ -123,13 +123,14 @@ class Game(object):
     def is_full(self):
         for i in range(4):
             for j in range(4):
-                if grid[i][j] == 0:
+                if self.grid[i][j] == 0:
                     return False
 
         return True
 
 
     def show_grid(self):
+        print("\n")
         for i in range(4):
             print(self.grid[i][0], self.grid[i][1], self.grid[i][2], self.grid[i][3])
         print("\n")
@@ -137,7 +138,7 @@ class Game(object):
     def get_current_state(self):
         for i in range(4):
             for j in range(4):
-                if grid[i][j] == 2048:
+                if self.grid[i][j] == 2048:
                     return "Won"
 
                 elif self.grid[i][j] == 0:
@@ -145,14 +146,17 @@ class Game(object):
 
         for i in range(3):
             for j in range(3):
-                if grid[i][j] == grid[i + 1][j] or grid[i][j] == grid[i][j + 1]:
+                if self.grid[i][j] == self.grid[i + 1][j] or self.grid[i][j] == self.grid[i][j + 1]:
                     return "Not done jet"
 
         for i in range(3):
-            if grid[i][3] == grid[i + 1][3]:
+            if self.grid[i][3] == self.grid[i + 1][3]:
                 return "Not done jet"
 
-            elif grid[3][i] == grid[3][i + 1]:
+            elif self.grid[3][i] == self.grid[3][i + 1]:
                 return "Not done jet"
 
         return "Game Over"
+
+if __name__ == "__main__":
+    game = Game()
